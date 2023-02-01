620508a7ddde12b51c96f7b0f561394f8cbb37eb
DucPV <phamvanduc.dev@gmail.com>
2023-02-01 08:58:41 +0700

    [Qodana] Basic configuration

---
+++ app/src/main/java/com/ducpv/movie/QodanaTest.kt
@@ -0,0 +1,14 @@
+package com.ducpv.movie
+
+class QodanaTest {
+    fun `PrivatePropertyName`() {
+        var My_Cool_Property = ""
+        My_Cool_Property = "a"
+        println(My_Cool_Property)
+    }
+
+    fun `ReplaceSizeZeroCheckWithIsEmpty`() {
+        val arrayOf = arrayOf(1, 2, 3)
+        println(arrayOf.size == 0)
+    }
+}
